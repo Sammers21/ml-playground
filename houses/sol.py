ed6d4fbb194437bdfebaad0763c4377a636ee268
Sammers21 <titantins@gmail.com>
2023-04-23 18:04:39 +0300

    initial commit

---
+++ houses/sol.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import GroupShuffleSplit
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+
+houses = pd.read_csv('train.csv')
