343ffd43d20aedf3f239abcaee2a9555f007fb11
Sammers21 <titantins@gmail.com>
2023-04-23 22:02:18 +0300

    improvements

--- houses/sol.py
+++ houses/sol.py
@@ -1,20 +1,40 @@
+from tensorflow.keras import callbacks
+from tensorflow.keras import layers
+from tensorflow import keras
+from sklearn.model_selection import GroupShuffleSplit, train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.style.use('seaborn-whitegrid')
-# Set Matplotlib defaults
-plt.rc('figure', autolayout=True)
-plt.rc('axes', labelweight='bold', labelsize='large',
-       titleweight='bold', titlesize=18, titlepad=10)
-plt.rc('animation', html='html5')
-
-
-import pandas as pd
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.model_selection import GroupShuffleSplit
-
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras import callbacks
-
-houses = pd.read_csv('train.csv')
+houses_train = pd.read_csv('train.csv')
+print(houses_train.head())
+print(houses_train.shape)
+X = houses_train.copy()
+Y = X.pop('SalePrice')
+X = X[['LotArea', 'YearBuilt', 'TotRmsAbvGrd']]
+print("X shape: ", X.shape)
+print("Y shape: ", Y.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y,
+    test_size=0.2,
+    random_state=42
+)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=[3]),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1)
+])
+model.compile(
+    optimizer='adam',
+    loss='mean_absolute_error',
+    metrics=['mean_absolute_error']
+)
+z = model.fit(
+    X_train, y_train,
+    validation_data=(X_test, y_test),
+    epochs=10,
+    batch_size=16,
+)
+plt.plot(z.history['loss'], label='loss')
